3821d4932bc6e15c86725b967def6f2c200cc6d2
Kevin Chavez <mr.kev@me.com>
2025-10-27 14:43:03 -0600

    add tailwindcss and react-router-dom to site

--- packages/site/src/main.tsx
+++ packages/site/src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./index.css";
+import { LinkedState } from "./LinkedState.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ls" element={<LinkedState />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "../../docs",
     emptyOutDir: true, // also necessary
